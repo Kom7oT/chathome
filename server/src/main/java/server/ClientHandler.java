b51e71c2f995c8f4229d07057b24d1f361580ef7
Kom7oT <86073918+Kom7oT@users.noreply.github.com>
2021-08-18 00:16:51 +0300

    hw2.8

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -13,6 +13,7 @@ public class ClientHandler {
 
     private boolean authenticated;
     private String nickname;
+    private String login;
 
     public ClientHandler(Socket socket, Server server) {
         try {
@@ -24,6 +25,7 @@ public class ClientHandler {
 
             new Thread(() -> {
                 try {
+                    //socket.setSoTimeout(0);
                     // цикл аутентификации
                     while (true) {
                         String str = in.readUTF();
@@ -37,13 +39,33 @@ public class ClientHandler {
                             String[] token = str.split("\\s+");
                             nickname = server.getAuthService()
                                     .getNicknameByLoginAndPassword(token[1], token[2]);
+                            login = token[1];
                             if (nickname != null) {
-                                server.subscribe(this);
-                                authenticated = true;
-                                sendMsg("/authok " + nickname);
-                                break;
+                                if (!server.isLoginAuthenticated(login)) {
+                                    sendMsg("/authok " + nickname);
+                                    server.subscribe(this);
+                                    authenticated = true;
+                                    break;
+                                } else {
+                                    sendMsg("С этим логином уже вошли");
+                                }
                             } else {
                                 sendMsg("Неверный логин / пароль");
+                            }
+                        }
+
+                        if (str.startsWith("/reg ")) {
+                            String[] token = str.split("\\s+");
+                            if (token.length < 4) {
+                                continue;
+                            }
+
+                            boolean regOk = server.getAuthService().
+                                    registration(token[1], token[2], token[3]);
+                            if (regOk) {
+                                sendMsg("/regok");
+                            } else {
+                                sendMsg("/regno");
                             }
                         }
                     }
@@ -57,6 +79,7 @@ public class ClientHandler {
                                 System.out.println("Client disconnected");
                                 break;
                             }
+
                             if (str.startsWith("/w")) {
                                 String[] token = str.split("\\s+", 3);
                                 if (token.length < 3) {
@@ -68,6 +91,7 @@ public class ClientHandler {
                             server.broadcastMsg(this, str);
                         }
                     }
+                // SocketTimeoutException
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
@@ -94,5 +118,9 @@ public class ClientHandler {
 
     public String getNickname() {
         return nickname;
+    }
+
+    public String getLogin() {
+        return login;
     }
 }
